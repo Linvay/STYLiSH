8224e3ce292215c5012779285c151a990acad11c
Linvay <63941955+Linvay@users.noreply.github.com>
2022-10-07 18:38:10 +0800

    fix: changed config db.config names

--- models/index.js
+++ models/index.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(
     process.env.DB,
-    process.env.USER,
-    process.env.PASSWORD,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
-        host: process.env.HOST,
-        dialect: process.env.DIALECT
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT
     }
 )
 
--- services/db.js
+++ services/db.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 
 async function getConnection() {
     return await mysql.createConnection({
-        host: process.env.HOST,
-        port: parseInt(process.env.PORT, 10),
-        user: process.env.USER,
-        password: process.env.PASSWORD,
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT, 10),
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
         database: process.env.DB,
         charset: process.env.CHARSET
     });
